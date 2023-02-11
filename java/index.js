597494922cba2af24f8904a284acd0aa28250a76
M-Muiz <122803576+M-Muiz@users.noreply.github.com>
2023-02-11 15:12:16 +0500

    9-11

---
+++ java/index.js
@@ -0,0 +1,231 @@
+Q:1
+
+// let a = prompt("Enter your city");
+
+// if(a == "karachi"){
+// alert("Welcome to city of lights")
+// }
+
+Q:2
+
+// let b = prompt("Enter your grnder");
+
+// if(b == "male"){
+// alert("Good Morning Sir")
+// }
+
+// else if(b == "female"){
+// alert("Good Morning mam")
+// }
+
+// else{
+//     alert("Good Morning")
+// }
+
+
+// Q:3
+// let colour = prompt("Enter a colour");
+
+// if(colour == "red"){
+// alert("Must stop")
+// }
+
+
+// else if(colour == "yellow"){
+// alert("Ready to move")
+// }
+
+// else if(colour == "green"){
+// alert("Move now")
+// }
+
+
+Q:4
+// let pet = prompt("Enter a remaining fuel");
+
+// if (pet <= 0.25){
+//     alert("Please refill the fuel in your car")
+// }
+
+Q:5
+/*
+   var a = 4;
+   if (++a === 5){
+    alert("given condition for variable a is true");
+}
+   yes it works
+
+   b
+var b = 82;
+if (b++ === 83){
+    alert("given condition for variable b is true");
+}
+not working
+
+var c = 12;
+if (c++ === 13){
+    alert("condition 1 is true");
+}
+not works
+if (c === 13){
+    alert("condition 2 is true");
+}
+it works
+if (++c < 14){
+    alert("condition 3 is true");
+}
+not works
+if(c === 14){
+    alert("condition 4 is true");
+}
+works
+
+d
+
+var materialCost = 20000;
+
+var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+
+if (totalCost === laborCost + materialCost){
+ alert("The cost equals");
+
+}
+
+works
+
+e
+ if (true){
+         alert("True");
+    }
+ if (false){
+         alert("False");
+ }
+     woks
+
+//f
+// if("car" < "cat"){
+//     alert("car is smaller than cat");
+//     } 
+ working   
+
+ */
+
+// Q:6
+// let mark = +prompt("Enter Subject 1 mark")
+// let mark1 = +prompt("Enter Subject 2 mark")
+// let mark2 = +prompt("Enter Subject 3 mark")
+
+// let total = (mark + mark1 + mark2);
+// alert(total);
+// console.log(total);
+// var main = 300
+// var percentage = (total * 100)/(main); 
+// console.log (percentage);
+// if(percentage <40){
+//     alert("Fail")
+// }
+
+// else if(percentage >45){
+//     alert("Grade D")
+// }
+
+// else if(percentage >55){
+//     alert("Grade C")
+// }
+
+// else if(percentage >60){
+//     alert("Grade B")
+// }
+
+// else if(percentage >=70){
+//     alert("Grade A")
+// }
+
+// else if(percentage >=80){
+//     alert("Grade A+")
+// }
+
+
+
+Q:7
+
+// let secret = prompt("Guess the number");
+//  var number = 4
+// if(secret == number){
+//     alert("Bingo! Correct Answer" )
+// }
+
+// else if (secret ==++number){
+// alert("Close enough to the correct answer")
+// }
+
+// else{
+//     alert ("Try again")
+// }
+
+
+Q:8
+
+// let num2 =+prompt("Enter a Number")
+
+// if(num2%3){
+//     alert("The number is not divisible by 3")
+// }
+
+// else{
+//     alert("The number is divisible by 3")
+// }
+
+Q:9
+
+// var num1=+prompt("Enter a number")
+// if(num1%2==0){
+//     alert("the number is even")
+// }
+// else{
+//     alert("the number is odd")
+// }
+
+Q:10
+// let temp = prompt ("write the temperature");
+
+// if (temp >=40){
+//     alert("It is too hot outside")
+// }
+
+// else if (temp >=30){
+//     alert("The weather is Normal")
+// }
+
+// else if (temp >=20){
+//     alert("Today's weather is cool")
+// }
+
+// else if (temp >=10){
+//     alert("OMG! Today's weather is so cool")
+// }
+
+Q:11
+
+// let num1 = +prompt("Enter first number")
+// let opr = prompt("Enter a oprator")
+// let num2 = +prompt("Enter second number")
+
+// if(opr == "+"){
+//     alert(num1 + num2)
+// }
+
+// if(opr == "-"){
+//     alert(num1 - num2)
+// }
+
+// if(opr == "*"){
+//     alert(num1 * num2)
+// }
+
+// if(opr == "/"){
+//     alert(num1 / num2)
+// }
+
+
